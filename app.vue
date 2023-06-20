3b6189d9be449f4554c45901fdca7104bcf4f95f
Kevin Espaldon <kevin.espaldon@ctechm.com>
2023-06-20 08:54:41 +0400

    init and only commit

---
+++ app.vue
@@ -0,0 +1,10 @@
+<template>
+  <div>
+    <div id="videoContext"></div>
+  </div>
+</template>
+
+<script setup>
+import * as voplayer from "voplayer-html5";
+const videoContext = ref(null);
+</script>
